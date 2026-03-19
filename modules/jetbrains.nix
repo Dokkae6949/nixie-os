67fbe6771f5ff393c87dc12e676fc128888c7584
Dokkae6949 <finnliry@gmail.com>
2026-03-19 11:18:34 +0000

    fix: idea ide

--- modules/jetbrains.nix
+++ modules/jetbrains.nix
@@ -4,14 +4,18 @@
   nixie.jetbrains= {
     description = "JetBrains IDEs";
 
-    home = { pkgs, ... }: {
-      home.packages = let
-        inherit (pkgs.jetbrains) idea;
-        plugins = inputs.nix-jetbrains-plugins.lib.pluginsForIde pkgs idea [
-          "com.github.copilot"
-        ];
-      in [
-        (pkgs.jetbrains.plugins.addPlugins idea (lib.attrValues plugins))
+    home = { lib, pkgs, ... }: {
+      # home.packages = let
+      #   inherit (pkgs.jetbrains) idea;
+      #   plugins = inputs.nix-jetbrains-plugins.lib.pluginsForIde pkgs idea [
+      #     "com.github.copilot"
+      #   ];
+      # in [
+      #   (pkgs.jetbrains.plugins.addPlugins idea (lib.attrValues plugins))
+      # ];
+
+      home.packages = with inputs.nix-jetbrains-plugins.lib; [
+        (buildIdeWithPlugins pkgs "idea" ["com.github.copilot"])
       ];
     };
   };
