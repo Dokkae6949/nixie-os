4ae52fbcbb80aef334eae5ddf9ef8fb917373fbc
Dokkae6949 <finnliry@gmail.com>
2026-09-05 11:48:01 +0200

    Remove postgres from host

--- modules/profiles/development.nix
+++ modules/profiles/development.nix
@@ -4,7 +4,6 @@
   flake.modules.nixos.development.imports = with config.flake.modules.nixos; [
     docker
     nix-ld
-    postgresql
   ];
 
   flake.modules.homeManager.development.imports = with config.flake.modules.homeManager; [
